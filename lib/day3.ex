1fe56780ad3fde44e3aacf9f0c5614ff537a14cc
Jeffrey Shen <jeffrey.shen@kauza.dk>
2025-12-10 14:50:46 +0100

    feat: day3 part 2

--- lib/day3.ex
+++ lib/day3.ex
@@ -16,23 +16,41 @@ defmodule Day3 do
       |> Enum.sum()
   end
 
-  @spec find_largest_pair(List[String.t()], String.t()) :: integer()
-  def find_largest_pair(line, curr_max) when length(line) == 1 do
-    numify(curr_max,hd(line))
+  def part_b do
+    parse_part_a()
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(fn line -> find_largest_n(line, 12) end)
+    |> Enum.map(&numify_list/1)
+    |> Enum.sum()
   end
 
   @spec find_largest_pair(List[String.t()], String.t()) :: integer()
   def find_largest_pair(line, curr_max) do
-    [head | rest] = line
-    max(find_largest_pair(rest, max(curr_max, head)), numify(curr_max, head))
+    case line do
+      [head] -> numify(curr_max, head)
+      [head | rest] -> max(find_largest_pair(rest, max(curr_max, head)), numify(curr_max, head))
+    end
   end
 
   def numify(a, b) do
     String.to_integer(a) * 10  + String.to_integer(b)
   end
 
-  def part_b do
-    # parse_part_a()
+  def numify_list(l) when is_list(l) do
+    Enum.map(l, &String.to_integer/1)
+    |> Enum.reduce(fn x, acc -> acc * 10 + x end)
+  end
+
+  @spec find_largest_n(List[String.t()], integer()) :: List[String.t()]
+  def find_largest_n(line, n) do
+    cond do
+      n == 1 -> [Enum.max(line)]
+      length(line) === n -> line
+      true ->
+        [head | rest] = line
+        curr_max = find_largest_n(rest, n)
+        if(hd(curr_max) > head, do: curr_max, else: [head | find_largest_n(curr_max, n-1)])
+    end
   end
 
   def parse_part_a() do
