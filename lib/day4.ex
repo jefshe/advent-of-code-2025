37d5e4de2dde6b7cac5c7006560355c00263997d
Jeffrey Shen <jeffrey.shen@kauza.dk>
2025-12-10 18:13:29 +0100

    feat: tidy

--- lib/day4.ex
+++ lib/day4.ex
@@ -11,7 +11,10 @@ defmodule Day4 do
 
   def part_a do
     grid = parse_part_a()
-    grid[x: 3][y: 0]
+    {x_size, y_size} = Nx.shape(grid.shape)
+    for y <- 0..(y_size-1), x<-0..(x_size-1) do
+      IO.puts(Nx.to_number(grid[x: x][y: y]))
+    end
   end
 
   def part_b do
@@ -22,6 +25,7 @@ defmodule Day4 do
   def parse_part_a() do
     Utils.read_file_stream(4, :ex)
     |> Enum.map(&String.to_charlist/1)
+    |> Enum.map(&Enum.map(&1, fn c -> if c == ?. do 0 else 1 end end))
     |> Nx.tensor(names: [:y, :x])
   end
 end
