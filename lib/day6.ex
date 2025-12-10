16a22a28c6f2cac05078d1047dc59b88241d0a66
jefshe <jeffrey.shen@kauza.dk>
2025-12-11 00:27:36 +0100

    feat: day 6 part 2

--- lib/day6.ex
+++ lib/day6.ex
@@ -17,6 +17,8 @@ defmodule Day6 do
 
   def part_b do
     parse_part_b(:a)
+    |> Enum.map(fn {args, op} -> Enum.reduce(args, op) end)
+    |> Enum.sum()
   end
 
   def string_to_op(op) do
@@ -26,7 +28,11 @@ defmodule Day6 do
     end
   end
 
-  @spec parse_part_a(:a | :b | :ex) :: [{[integer()], String.t()}]
+  def string_to_int(list) do
+    Enum.join(list) |> String.trim() |> String.to_integer()
+  end
+
+  @spec parse_part_a(:a | :b | :ex) :: [{[integer()], function()}]
   def parse_part_a(part) do
     Utils.read_file_stream(6, part)
     |> Enum.map(&String.split/1)
@@ -34,5 +40,31 @@ defmodule Day6 do
     |> Enum.map(fn {a, b, c, d, op} ->
       {Enum.map([a, b, c, d], &String.to_integer/1), string_to_op(op)}
     end)
+  end
+
+  @spec parse_part_b(:a | :b | :ex) :: [{[integer()], function()}]
+  def parse_part_b(part) do
+    Utils.read_file_stream_ws(6, part)
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.zip()
+    |> Enum.reject(fn x -> Enum.all?(Tuple.to_list(x), &(&1 == " ")) end)
+    |> Enum.chunk_while(
+      [],
+      fn col, acc ->
+        case {col, acc} do
+          {{a, b, c, d, op}, []} ->
+            {:cont, [string_to_int([a, b, c, d]), op]}
+
+          {{a, b, c, d, op}, _} ->
+            if(op != " ",
+              do: {:cont, acc, [string_to_int([a, b, c, d]), op]},
+              else: {:cont, [string_to_int([a, b, c, d]) | acc]}
+            )
+        end
+      end,
+      &{:cont, &1, &1}
+    )
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.map(fn [op | nums] -> {nums, string_to_op(op)} end)
   end
 end
--- lib/utils.ex
+++ lib/utils.ex
@@ -3,8 +3,13 @@ defmodule Utils do
     {:ok, file} = File.read("inputs/day#{day}/#{part}.txt")
     file
   end
+
   def read_file_stream(day, part, lines_or_bytes \\ :line) do
     File.stream!("inputs/day#{day}/#{part}.txt", lines_or_bytes)
     |> Stream.map(&String.trim/1)
+  end
+
+  def read_file_stream_ws(day, part, lines_or_bytes \\ :line) do
+    File.stream!("inputs/day#{day}/#{part}.txt", lines_or_bytes)
   end
 end
