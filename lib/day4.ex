ceef7ef242ed09c446ae2dc701624abff67e6ea2
jefshe <jeffrey.shen@kauza.dk>
2025-12-10 19:40:34 +0100

    feat: day4 part 1

--- lib/day4.ex
+++ lib/day4.ex
@@ -12,21 +12,46 @@ defmodule Day4 do
   def part_a do
     grid = parse_part_a()
     {x_size, y_size} = Nx.shape(grid.shape)
-    for y <- 0..(y_size-1), x<-0..(x_size-1) do
-      IO.puts("#{x}, #{y}: #{grid[x: x][y: y]}")
-    end
-    Nx.to_heatmap(grid)
+    {x_max, y_max} = {x_size - 1, y_size - 1}
+
+    found =
+      for y <- 0..y_max, x <- 0..x_max do
+        if Nx.to_number(grid[x: x][y: y]) == 1 do
+          if(
+            grid[x: max(x - 1, 0)..min(x + 1, x_max)][
+              y: max(y - 1, 0)..min(y + 1, y_max)
+            ]
+            |> Nx.sum()
+            |> Nx.to_number() <= 4,
+            do: 1,
+            else: 0
+          )
+        else
+          0
+        end
+      end
+
+    Enum.sum(found)
+
+    # Nx.to_heatmap(grid)
   end
 
   def part_b do
     # parse_part_a()
   end
 
-
   def parse_part_a() do
-    Utils.read_file_stream(4, :ex)
+    Utils.read_file_stream(4, :a)
     |> Enum.map(&String.to_charlist/1)
-    |> Enum.map(&Enum.map(&1, fn c -> if c == ?. do 0 else 1 end end))
+    |> Enum.map(
+      &Enum.map(&1, fn c ->
+        if c == ?. do
+          0
+        else
+          1
+        end
+      end)
+    )
     |> Nx.tensor(names: [:y, :x])
   end
 end
