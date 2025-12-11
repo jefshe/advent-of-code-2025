4e33a0666cdb46e2cc0111f1a17a1c2cb25bb521
jefshe <jeffrey.shen@kauza.dk>
2025-12-11 23:03:34 +0100

    feat: graph kind working

--- lib/day8.ex
+++ lib/day8.ex
@@ -10,10 +10,41 @@ defmodule Day8 do
   end
 
   def part_a do
-    parse_part(:ex)
+    pq = PriorityQueue.new()
+    connect_n = 10
+
+    pq =
+      parse_part(:a)
+      |> unique_pairs()
+      |> Enum.reduce(pq, fn {x, y}, acc ->
+        PriorityQueue.push(
+          acc,
+          {Nx.to_list(x), Nx.to_list(y)},
+          Nx.subtract(x, y) |> Nx.pow(2) |> Nx.sum() |> Nx.to_number()
+        )
+      end)
+
+    {graph, _} =
+      Enum.reduce(1..connect_n, {Graph.new(), pq}, fn _, {graph, pq} ->
+        add_connected_nodes(graph, pq)
+      end)
+
+    graph
+  end
+
+  def add_connected_nodes(graph, pq) do
+    {{:value, {a, b}}, new_pq} = PriorityQueue.pop(pq)
+    {Graph.add_edge(graph, a, b), new_pq}
   end
 
   def part_b do
+  end
+
+  def unique_pairs(list) do
+    for {x, i} <- Enum.with_index(list),
+        {y, j} <- Enum.with_index(list),
+        i < j,
+        do: {x, y}
   end
 
   @spec parse_part(:a | :b | :ex) :: [{integer(), integer(), integer()}]
@@ -21,6 +52,8 @@ defmodule Day8 do
     Utils.read_file_stream(8, part)
     |> Enum.map(&String.split(&1, ","))
     |> Enum.map(fn chars -> Enum.map(chars, &String.to_integer/1) end)
-    |> Enum.map(&List.to_tuple/1)
+    |> Enum.map(&Nx.tensor/1)
+
+    # |> Enum.map(&List.to_tuple/1)
   end
 end
